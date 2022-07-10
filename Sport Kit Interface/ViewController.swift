a7ee2b8c5c642262de2318418004689ed0b5904d
ungros <82379236+ungros@users.noreply.github.com>
2022-07-10 20:55:46 +0300

    Initial Commit

---
+++ Sport Kit Interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sport Kit Interface
+//
+//  Created by Roman on 10.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
