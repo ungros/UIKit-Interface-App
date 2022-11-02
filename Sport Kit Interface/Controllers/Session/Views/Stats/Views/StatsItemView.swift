92d2c344ef06dd24ce025532f73bd330fa9debc4
ungros <82379236+ungros@users.noreply.github.com>
2022-11-02 18:30:19 +0300

    Constraints

---
+++ Sport Kit Interface/Controllers/Session/Views/Stats/Views/StatsItemView.swift
@@ -0,0 +1,66 @@
+//
+//  StatItemView.swift
+//  Sport Kit Interface
+//
+//  Created by Roman on 30.10.2022.
+//
+
+import UIKit
+
+final class StatsIemView: BaseView {
+    
+    private let imagView = UIImageView()
+    
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.font = R.Fonts.helvelticaRegular(with: 17)
+        label.textColor = R.Colors.titleGray
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = R.Fonts.helvelticaRegular(with: 10)
+        label.textColor = R.Colors.inactive
+        return label
+    }()
+    
+    private let stackViev: UIStackView = {
+        let viev = UIStackView()
+        viev.axis = .vertical
+        return viev
+    }()
+    
+}
+
+extension StatsIemView {
+   
+    override func setupViews(){
+        super.setupViews()
+        
+        setupView(imagView)
+        setupView(stackViev)
+        stackViev.addArrangedSubview(valueLabel)
+        stackViev.addArrangedSubview(titleLabel)
+    }
+   
+    override func constaintViews(){
+        super.constaintViews()
+        
+        NSLayoutConstraint.activate([
+            imagView.topAnchor.constraint(equalTo: centerYAnchor),
+            imagView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imagView.heightAnchor.constraint(equalToConstant: 23),
+            imagView.widthAnchor.constraint(equalTo: imagView.heightAnchor),
+            
+            stackViev.leadingAnchor.constraint(equalTo: imagView.trailingAnchor, constant: 15),
+            stackViev.topAnchor.constraint(equalTo: topAnchor),
+            stackViev.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackViev.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    override func configureAppearance(){
+        super.configureAppearance()
+    }
+}
